5b1c6ba4e42e432d95988e1b3b908944ab8ddaa7
Le Van Pon <spixai2020@gmail.com>
2021-08-27 16:24:59 +0700

    update

--- next.config.js
+++ next.config.js
@@ -8,32 +8,8 @@ const regexEqual = (x, y) =>
 
 module.exports = {
     env: {
-        title: 'HMSP International',
-        titleDescription: 'HMSP International',
+        title: 'Bluestone',
+        titleDescription: 'Bluestone',
     },
-    webpack(config) {
-        const sassRules = config.module.rules
-            .find((rule) => typeof rule.oneOf === "object")
-            .oneOf.find((rule) =>
-                    rule.sideEffects === false &&
-                    (regexEqual(rule.test, /\.module\.(scss|sass)$/) ||
-                    regexEqual(rule.test, /\.module\.css$/))
-            );
-
-        sassRules.use = sassRules.use.map((rule) =>
-            rule.loader.includes("css-loader/dist")
-                ? {
-                    ...rule,
-                    options: {
-                        ...rule.options,
-                        modules: {
-                            ...rule.modules,
-                            localIdentName: "[hash:base64:5]",
-                        },
-                    },
-                }
-                : rule
-        );
-        return config;
-    },
+    target: "serverless"
 };
