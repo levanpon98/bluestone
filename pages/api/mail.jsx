cf2bf146f464de72ef105590247d06d64ce866b5
Le Van Pon <spixai2020@gmail.com>
2021-08-31 15:28:19 +0700

    update

--- pages/api/mail.jsx
+++ pages/api/mail.jsx
@@ -27,8 +27,8 @@ export default async (req, res) => {
     // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
         host: "gsgpm1018.siteground.biz",
-        port: 465,
-        secure: true, // true for 465, false for other ports
+        port: 567,
+        secure: false, // true for 465, false for other ports
         auth: {
             user: process.env.CUSTOMER_EMAIL, // generated ethereal user
             pass: process.env.CUSTOMER_PASSWORD, // generated ethereal password
