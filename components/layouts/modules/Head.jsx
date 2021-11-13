a1d5720a9e36240a9a8a0a861fe905bd6a9459df
Le Van Pon <spixai2020@gmail.com>
2021-11-13 12:09:12 +0700

    update

--- components/layouts/modules/Head.jsx
+++ components/layouts/modules/Head.jsx
@@ -4,10 +4,10 @@ import Head from 'next/head';
 const StyleSheets = () => (
     <Head>
         <title>BLUESTONE BUYERS AGENTS</title>
-        <link rel="shortcut icon" href="/static/img/favi.png" />
-        <link rel="icon" href="/static/img/favi.png" sizes="32x32" />
-        <link rel="icon" href="/static/img/favi.png" sizes="192x192" />
-        <link rel="apple-touch-icon-precomposed" href="/static/img/favi.png" />
+        <link rel="shortcut icon" href="/Favicon/Favicon 32x32.png" />
+        <link rel="icon" href="/Favicon/Favicon 32x32.png" sizes="32x32" />
+        <link rel="icon" href="/Favicon/Favicon 64x64.png" sizes="64x64" />
+        <link rel="apple-touch-icon-precomposed" href="/Favicon/Favicon 32x32.png" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="format-detection" content="telephone=no" />
